113223eeb6633f964b278ca5abe810ed6425011e
Ethan Zhang <ezhang3333@gmail.com>
2025-12-16 18:33:30 -0600

    Tuned pfr def vs cleaner calculations

--- data_cleaners/pfr_def_cleaner.py
+++ data_cleaners/pfr_def_cleaner.py
@@ -38,10 +38,12 @@ class PFRDefCleaner:
         return rb_def
     
     def calculate_def_vs_te(self, def_vs_te):
+        def_vs_te = def_vs_te.copy()
+
         def_vs_te["def_te_ftps"] = def_vs_te["fantpt_per_game"]
         def_vs_te["def_te_targets"] = def_vs_te["rec_tgts"] / def_vs_te["games"]
         def_vs_te["def_te_yards_per_target"] = def_vs_te["rec_yds"] / def_vs_te["rec_tgts"]
-        def_vs_te["team_abbev"] = def_vs_te["team"].map(TEAM_NAME_TO_ABBR)
+        def_vs_te["team_abbrev"] = def_vs_te["team"].map(TEAM_NAME_TO_ABBR)
 
         te_def = def_vs_te[[
             "team_abbrev",
@@ -52,16 +54,18 @@ class PFRDefCleaner:
         return te_def
     
     def calculate_def_vs_qb(self, def_vs_qb):
+        def_vs_qb = def_vs_qb.copy()
+
         def_vs_qb["def_pass_ypa_allowed"] = def_vs_qb["pass_yds"] / def_vs_qb["pass_att"]
-        def_vs_qb["def_total_tds_allowed"] = def_vs_qb["rush_tds"] + def_vs_qb["rec_tds"] / def_vs_qb["games"]
+        def_vs_qb["def_total_tds_allowed"] = (def_vs_qb["rush_tds"] + def_vs_qb["pass_tds"]) / def_vs_qb["games"]
         def_vs_qb["def_pass_int_rate_forced"] = def_vs_qb["pass_int"] / def_vs_qb["games"]
         def_vs_qb["def_sack_rate"] = def_vs_qb["sacks"] / def_vs_qb["games"]
-        def_vs_qb["def_fantpy_allowed"] = def_vs_qb["fantpt_per_game"]
+        def_vs_qb["def_fantpt_allowed"] = def_vs_qb["fantpt_per_game"]
         def_vs_qb["def_rush_ypa_allowed"] = def_vs_qb["rush_yds"] / def_vs_qb["rush_att"]
-        def_vs_qb["team_abbev"] = def_vs_qb["team"].map(TEAM_NAME_TO_ABBR)
+        def_vs_qb["team_abbrev"] = def_vs_qb["team"].map(TEAM_NAME_TO_ABBR)
 
         qb_def = def_vs_qb[[
-            "team_abbev",
+            "team_abbrev",
             "def_pass_ypa_allowed",
             "def_total_tds_allowed",
             "def_pass_int_rate_forced",
@@ -72,10 +76,12 @@ class PFRDefCleaner:
         return qb_def
     
     def calculate_def_vs_wr(self, def_vs_wr):
+        def_vs_wr = def_vs_wr.copy()
+
         def_vs_wr["def_wr_ftps"] = def_vs_wr["fantpt_per_game"]
         def_vs_wr["def_wr_targets"] = def_vs_wr["rec_tgts"] / def_vs_wr["games"]
         def_vs_wr["def_wr_yards_per_target"] = def_vs_wr["rec_yds"] / def_vs_wr["rec_tgts"]
-        def_vs_wr["team_abbev"] = def_vs_wr["team"].map(TEAM_NAME_TO_ABBR)
+        def_vs_wr["team_abbrev"] = def_vs_wr["team"].map(TEAM_NAME_TO_ABBR)
 
         wr_def = def_vs_wr[[
             "team_abbrev",
@@ -85,6 +91,7 @@ class PFRDefCleaner:
         ]].copy()
         return wr_def
     
+    # legacy qb def stats
     def calculate_qb_def_stats(self):
         team = self.team_def_stats[self.calculate_stats["team_def_stats"]].copy()
         adv  = self.adv_def_stats[self.calculate_stats["adv_def_stats"]].copy()
--- nfl_pipeline.py
+++ nfl_pipeline.py
@@ -3,9 +3,18 @@ from data_extractors.nfl_stats_web_scraper import NFLWebScraper
 from data_cleaners.pfr_def_cleaner import PFRDefCleaner
 
 web_scraper = NFLWebScraper()
-def_vs = web_scraper.pfr_scrape_def_vs_stats("2025", "RB")
+def_vs_rb = web_scraper.pfr_scrape_def_vs_stats("2025", "RB")
+def_vs_te = web_scraper.pfr_scrape_def_vs_stats("2025", "TE")
+def_vs_qb = web_scraper.pfr_scrape_def_vs_stats("2025", "QB")
+def_vs_wr = web_scraper.pfr_scrape_def_vs_stats("2025", "WR")
 
 pfr_def_cleaner = PFRDefCleaner()
-cleaned_def_vs = pfr_def_cleaner.calculate_def_vs_rb(def_vs)
-print(cleaned_def_vs)
+cleaned_def_vs_rb = pfr_def_cleaner.calculate_def_vs_rb(def_vs_rb)
+cleaned_def_vs_te = pfr_def_cleaner.calculate_def_vs_te(def_vs_te)
+cleaned_def_vs_qb = pfr_def_cleaner.calculate_def_vs_qb(def_vs_qb)
+cleaned_def_vs_wr = pfr_def_cleaner.calculate_def_vs_wr(def_vs_wr)
 
+print(cleaned_def_vs_rb)
+print(cleaned_def_vs_te)
+print(cleaned_def_vs_qb)
+print(cleaned_def_vs_wr)
