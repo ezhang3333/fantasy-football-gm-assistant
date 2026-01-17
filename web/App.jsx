79091291df8bea5194c7e15b851faea2d9cde659
Ethan Zhang <ezhang3333@gmail.com>
2026-01-17 04:29:12 -0500

    Added sidebar for model fine tuning and history

--- web/App.jsx
+++ web/App.jsx
@@ -1,5 +1,14 @@
+import "./App.css"
+
 export default function App(){
     return (
-        <div>Hello World!</div>
+        <div className='base-container'>
+            <div className="filter-and-history-sidebar">
+                <span>sidebar</span>
+            </div>
+            <div className="output-container">
+                <h3>Fantasy Football Predictor: Gradient Boosted Tree Model</h3>
+            </div>
+        </div>
     )
 }
